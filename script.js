a242ade260b8446a64c17e4caff7c446b473a44c
Kavinkumar1998 <kavinajith412@gmail.com>
2022-12-05 15:04:42 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //1. Get all the countries from the Asia continent /region using the Filter function
-
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all",true)
 request.send()
@@ -11,9 +10,8 @@ request.send()
     var res1 =res.map((ele)=>ele.name.common)
     console.log(res1);
  }
-
+//--------------------------------------------------------------------------------------
 // 2. Get all the countries with a population of less than 2 lakhs using Filter function
-
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all",true)
 request.send()
@@ -25,8 +23,8 @@ request.send()
     var res1 =res.map((ele)=>ele.name.common)
     console.log(res1);
  }
+ //------------------------------------------------------------------------------------------------------
 // 3.Print the following details name, capital, flag using forEach function
-
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all",true)
 request.send()
@@ -35,12 +33,8 @@ request.send()
     var result = JSON.parse(data)
     var res = result.forEach((ele)=>{console.log(`${ele.name.common} ${ele.capital} ${ele.flag}`)})
     }
-
-
-
-
+//---------------------------------------------------------------------------------------------
 // 4.Print the total population of countries using reduce function
-
     var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all",true)
 request.send()
